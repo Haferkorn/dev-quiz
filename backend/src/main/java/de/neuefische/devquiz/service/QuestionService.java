e2332cd3d3d21fa8f1d3fee9950cdcb2849a4d31
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-10-18 10:47:48 +0200

    Add Exeption if no question with ID

--- backend/src/main/java/de/neuefische/devquiz/service/QuestionService.java
+++ backend/src/main/java/de/neuefische/devquiz/service/QuestionService.java
@@ -41,15 +41,19 @@ public class QuestionService {
 
     public ValidationInfo validateQuestion(ValidationInfo infosFromFrontend) {
             Optional<Question> questionToCheck = questionRepo.findById(infosFromFrontend.getQuestionID());
-            List<Answer> allAnswers = questionToCheck.get().getAnswers();
-            ValidationInfo validationAnswer = new ValidationInfo(infosFromFrontend.getQuestionID(), null);
-            for (Answer answer : allAnswers) {
-                if (answer.isCorrect()) {
-                    validationAnswer.setAnswerID(answer.getId());
+            if(questionToCheck.isPresent()){
+                List<Answer> allAnswers = questionToCheck.get().getAnswers();
+                ValidationInfo validationAnswer = new ValidationInfo(infosFromFrontend.getQuestionID(), null);
+                for (Answer answer : allAnswers) {
+                    if (answer.isCorrect()) {
+                        validationAnswer.setAnswerID(answer.getId());
 
+                    }
                 }
-            }
-            return validationAnswer;
+                return validationAnswer;
+
+            }throw new NoSuchElementException("There is no Question with ID: "+ infosFromFrontend.getQuestionID());
+
         }
 
 }
